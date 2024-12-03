6995820c9ef1a330a00107ad63263d9253d08540
Jone501 <sukhoveev.roma@gmail.com>
2024-12-04 09:18:22 +1000

    Initial commit

---
+++ src/main/java/me/jone501/joneapi/JoneAPI.java
@@ -0,0 +1,15 @@
+package me.jone501.joneapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JoneAPI extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
